003a3e7da12396eb3e0dd3bb2ac354caa776f2a4
Denis Rodionov <nicholas.hawkwood@gmail.com>
2023-02-01 11:39:41 +0200

    add helpers

--- internal/utils/helpers.go
+++ internal/utils/helpers.go
@@ -28,14 +28,51 @@ func CenturyEqual(year1 int, year2 int) bool {
 // Start year and dash are always present.
 // It's supposed to be used for lifespans, meaning we always have birth, but may not have death.
 func FormatYearsRangeString(startYear int, finishYear int) string {
-	if !IsValidYear(startYear) {
+	if !IsValidYear(startYear) && !IsValidYear(finishYear) {
 		return ""
 	}
 	if !IsValidYear(finishYear) {
 		return fmt.Sprintf("%d–", startYear)
 	}
+	if !IsValidYear(startYear) {
+		return fmt.Sprintf("%d", finishYear)
+	}
 	if CenturyEqual(startYear, finishYear) {
 		return fmt.Sprintf("%d–%s", startYear, SliceYear(finishYear))
 	}
 	return fmt.Sprintf("%d–%d", startYear, finishYear)
+}
+
+// FormatWorkLength formats minutes into a string with hours and minutes, like "2h 35m"
+func FormatWorkLength(lengthInMinutes int) string {
+	hours := lengthInMinutes / 60
+	minutes := lengthInMinutes % 60
+	if hours == 0 && minutes == 0 {
+		return ""
+	}
+	if hours < 0 || minutes < 0 {
+		return ""
+	}
+	if hours == 0 {
+		return fmt.Sprintf("%dm", minutes)
+	}
+	if minutes == 0 {
+		return fmt.Sprintf("%dh", hours)
+	}
+	return fmt.Sprintf("%dh %dm", hours, minutes)
+}
+
+// FormatCatalogueName formats catalogue name of the musical work, like "BWV 12p".
+func FormatCatalogueName(catalogueName string, catalogueNumber int, cataloguePostfix string) string {
+	if catalogueName == "" || catalogueNumber == 0 {
+		return ""
+	}
+	return fmt.Sprintf("%s %d%s", catalogueName, catalogueNumber, cataloguePostfix)
+}
+
+func FormatWorkName(workTitle string, workNo int, workNickname string) string {
+	if workNickname == "" {
+		return fmt.Sprintf("%s No. %d", workTitle, workNo)
+	}
+	return fmt.Sprintf("%s No. %d&nbsp;<em>%s</em>", workTitle, workNo, workNickname)
 }
--- internal/utils/helpers_test.go
+++ internal/utils/helpers_test.go
@@ -6,39 +6,59 @@ import (
 )
 
 func TestIsValidYearIdentifiesValidYear(t *testing.T) {
-	assert.Equal(t, IsValidYear(1000), true)
-	assert.Equal(t, IsValidYear(1234), true)
-	assert.Equal(t, IsValidYear(9999), true)
+	assert.Equal(t, true, IsValidYear(1000))
+	assert.Equal(t, true, IsValidYear(1234))
+	assert.Equal(t, true, IsValidYear(9999))
 }
 
 func TestIsValidYearIdentifiesInvalidYear(t *testing.T) {
-	assert.Equal(t, IsValidYear(999), false)
-	assert.Equal(t, IsValidYear(10000), false)
-	assert.Equal(t, IsValidYear(0), false)
-	assert.Equal(t, IsValidYear(-1), false)
+	assert.Equal(t, false, IsValidYear(999))
+	assert.Equal(t, false, IsValidYear(10000))
+	assert.Equal(t, false, IsValidYear(0))
+	assert.Equal(t, false, IsValidYear(-1))
 }
 
 func TestSliceYearCreatesValidSlice(t *testing.T) {
-	assert.Equal(t, SliceYear(1985), "85")
-	assert.Equal(t, SliceYear(9999), "99")
+	assert.Equal(t, "85", SliceYear(1985))
+	assert.Equal(t, "99", SliceYear(9999))
 }
 
 func TestCenturyEqualReturnsTrueForEqualCenturies(t *testing.T) {
-	assert.Equal(t, CenturyEqual(1700, 1799), true)
-	assert.Equal(t, CenturyEqual(1750, 1749), true)
+	assert.Equal(t, true, CenturyEqual(1700, 1799))
+	assert.Equal(t, true, CenturyEqual(1750, 1749))
 }
 
 func TestCenturyEqualReturnsFalseForNonEqualCenturies(t *testing.T) {
-	assert.Equal(t, CenturyEqual(1699, 1700), false)
-	assert.Equal(t, CenturyEqual(1799, 1800), false)
-	assert.Equal(t, CenturyEqual(1200, 1500), false)
+	assert.Equal(t, false, CenturyEqual(1699, 1700))
+	assert.Equal(t, false, CenturyEqual(1799, 1800))
+	assert.Equal(t, false, CenturyEqual(1200, 1500))
+	assert.Equal(t, false, CenturyEqual(1, 2))
 }
 
 func TestFormatYearsRangeStringFormatsYearsRangeProperly(t *testing.T) {
-	assert.Equal(t, FormatYearsRangeString(1900, 1902), "1900–02")
-	assert.Equal(t, FormatYearsRangeString(1890, 1912), "1890–1912")
-	assert.Equal(t, FormatYearsRangeString(1890, 1), "1890–")
-	assert.Equal(t, FormatYearsRangeString(1990, 0), "1990–")
-	assert.Equal(t, FormatYearsRangeString(1, 1912), "")
-	assert.Equal(t, FormatYearsRangeString(-1, 0), "")
+	assert.Equal(t, "1900–02", FormatYearsRangeString(1900, 1902))
+	assert.Equal(t, "1890–1912", FormatYearsRangeString(1890, 1912))
+	assert.Equal(t, "1890–", FormatYearsRangeString(1890, 1))
+	assert.Equal(t, "1990–", FormatYearsRangeString(1990, 0))
+	assert.Equal(t, "1950", FormatYearsRangeString(0, 1950))
+	assert.Equal(t, "1912", FormatYearsRangeString(1, 1912))
+	assert.Equal(t, "", FormatYearsRangeString(-1, 0))
+}
+
+func TestFormatWorkLengthFormatsProperly(t *testing.T) {
+	assert.Equal(t, "12m", FormatWorkLength(12))
+	assert.Equal(t, "59m", FormatWorkLength(59))
+	assert.Equal(t, "1h", FormatWorkLength(60))
+	assert.Equal(t, "1h 2m", FormatWorkLength(62))
+	assert.Equal(t, "2h 3m", FormatWorkLength(123))
+	assert.Equal(t, "", FormatWorkLength(-5))
+	assert.Equal(t, "", FormatWorkLength(0))
+}
+
+func TestFormatCatalogueName(t *testing.T) {
+	assert.Equal(t, "BWV 12m", FormatCatalogueName("BWV", 12, "m"))
+	assert.Equal(t, "BWV 12", FormatCatalogueName("BWV", 12, ""))
+	assert.Equal(t, "", FormatCatalogueName("", 12, ""))
+	assert.Equal(t, "", FormatCatalogueName("BWV", 0, ""))
+	assert.Equal(t, "", FormatCatalogueName("", 0, ""))
 }
