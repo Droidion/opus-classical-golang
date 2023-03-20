c759c048f5ae7a6333ffe1d050a3c447af3917ab
Masha <nasonkina@yandex.ru>
2023-03-21 02:30:20 +0300

    add theme.ts, add minor style fixes

--- esbuild.js
+++ esbuild.js
@@ -3,24 +3,24 @@
 import esbuild from "esbuild";
 import esbuildSvelte from "esbuild-svelte";
 import sveltePreprocess from "svelte-preprocess";
-import { compress } from 'esbuild-plugin-compress';
+import { compress } from "esbuild-plugin-compress";
 
 esbuild
-    .build({
-        entryPoints: ["scripts/main.ts"],
-        mainFields: ["svelte", "browser", "module", "main"],
-        bundle: true,
-        outdir: "static/scripts",
-        minify: true,
-        write: false,
-        plugins: [
-            esbuildSvelte({
-                preprocess: sveltePreprocess(),
-            }),
-            compress({
-                outputDir: '',
-            })
-        ],
-        logLevel: "info",
-    })
-    .catch(() => process.exit(1))
+  .build({
+    entryPoints: ["scripts/main.ts", "scripts/theme.ts"],
+    mainFields: ["svelte", "browser", "module", "main"],
+    bundle: true,
+    outdir: "static/scripts",
+    minify: true,
+    write: false,
+    plugins: [
+      esbuildSvelte({
+        preprocess: sveltePreprocess(),
+      }),
+      compress({
+        outputDir: "",
+      }),
+    ],
+    logLevel: "info",
+  })
+  .catch(() => process.exit(1));
